520d301c6c626dbf3f4b82ae458af8de4fecbf01
samuelludwig <samuelludwig74@yahoo.com>
2018-12-29 16:27:46 -0500

    adds button_led file, a program to control an LED with a button

---
+++ PiFiles/button_led.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(25, GPIO.IN)
+
+while True:
+    if GPIO.input(25):
+        GPIO.output(18, False)
+    else:
+        GPIO.output(18, True)
